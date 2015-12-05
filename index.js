f46b227ab7a8ed5aafc8079862fafbab3e629931
Dakota Lightning <im@dakotalightning.com>
2015-12-04 20:58:01 -0800

    added updateOnlineStatus check

--- index.js
+++ index.js
@@ -1,33 +1,52 @@
-var app = require('app');
+const electron = require('electron');
+const app = electron.app; // Module to control application life.
+const ipcMain = electron.ipcMain;
+const BrowserWindow = electron.BrowserWindow; // Module to create native browser window.
 
-// var BrowserWindow = require('browser-window');
-//
-// var mainWindow = null;
-//
-// app.on('ready', function () {
-//     var mainWindow = new BrowserWindow({
-//         width: 1200,
-//         height: 800,
-//         show: false
-//     });
-//     mainWindow.loadUrl('file://' + __dirname + '/app/app.htm');
-// });
+// Report crashes to our server.
+electron.crashReporter.start();
 
-const BrowserWindow = require('browser-window');
+// Keep a global reference of the window object, if you don't, the window will
+// be closed automatically when the JavaScript object is garbage collected.
+var mainWindow = null;
 
-app.on('ready', function () {
-    var win = new BrowserWindow({
+// Quit when all windows are closed.
+app.on('window-all-closed', function() {
+    // On OS X it is common for applications and their menu bar
+    // to stay active until the user quits explicitly with Cmd + Q
+    if (process.platform != 'darwin') {
+        app.quit();
+    }
+});
+
+ipcMain.on('online-status-changed', function(event, status) {
+    console.log(status);
+});
+
+// This method will be called when Electron has finished
+// initialization and is ready to create browser windows.
+app.on('ready', function() {
+    // Create the browser window.
+    mainWindow = new BrowserWindow({
         width: 1200,
         height: 800,
         minWidth: 1200,
         minHeight: 800,
         show: false
     });
-    win.on('closed', function() {
-        win = null;
+
+    // and load the index.html of the app.
+    mainWindow.loadURL('file://' + __dirname + '/app/app.html');
+
+    // Open the DevTools.
+    mainWindow.webContents.openDevTools();
+    mainWindow.show();
+
+    // Emitted when the window is closed.
+    mainWindow.on('closed', function() {
+        // Dereference the window object, usually you would store windows
+        // in an array if your app supports multi windows, this is the time
+        // when you should delete the corresponding element.
+        mainWindow = null;
     });
-
-    win.loadURL('file://' + __dirname + '/app/app.html');
-    win.show();
-
 });
